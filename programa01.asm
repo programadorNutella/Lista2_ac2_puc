7c9ecb1f09c91004502013059817e4aef7e1bdc8
programadorNutella <l994992@mvrht.com>
2017-03-04 15:34:38 -0300

    Create programa01.asm

---
+++ programa01.asm
@@ -0,0 +1,11 @@
+# Lista de Arquitetura 2
+.text
+.globl main
+
+main :
+
+addi $s1,$0,10 		# $s1 = 10
+addi $s2,$0,-1		# $s2 = -1
+
+addi $s1,$s1,1		# $s1 = $s1 + 1
+add $s3,$s1,$s2	# $3 = $s1 + $s2
